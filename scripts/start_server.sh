6c9ec3d0f639fa053d3b0543e01d99b8d29af464
Priyanka Patel <83409681+Priyanka-Cloud-Infinite@users.noreply.github.com>
2021-09-02 14:19:43 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -17,30 +17,7 @@ do
 			npm run-script build
 			pm2 serve build $portno --spa
 			pm2 update
-			sudo tee -a  /etc/nginx/sites-available/\${arr1[\$i]}.conf >/dev/null << EOF
-        		server {
-			listen 80;
-			listen [::]:80;
-
-            		root /var/www/demo;
-
-			server_name $servname;
-			location / {
-                    		proxy_pass http://127.0.0.1:$portno/;
-				}
-			}
-        		server {
-			listen 443;
-			listen [::]:443;
-
-           		root /var/www/demo;
-
-			server_name $servname; 
-			location / {
-                        proxy_pass http://127.0.0.1:$portno/;
-				}
-			}               
-EOF
+			
 			cd
 		elif [ $type == node ]
 		then
@@ -51,35 +28,67 @@ EOF
 			cd ${arr1[$i]}
 			npm i
 			pm2 start $filename
-			sudo tee -a  /etc/nginx/sites-available/\${arr1[\$i]}.conf >/dev/null << EOF
-        		server {
-			listen 80;
-			listen [::]:80;
-
-            		root /var/www/demo;
-
-			server_name $servname;
-			location / {
-                    		proxy_pass http://127.0.0.1:$portno/;
-				}
-			}
-        		server {
-			listen 443;
-			listen [::]:443;
-
-           		root /var/www/demo;
-
-			server_name $servname; 
-			location / {
-                        proxy_pass http://127.0.0.1:$portno/;
-				}
-			}               
-EOF
+			pm2 update
 			cd
 		elif [ $type == html ]
 		then
 			servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
 			servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+			
+		else
+			echo "Not an app"
+		fi
+	fi
+done
+pm2 save --force
+	for(( i=0; i<\$total; i++ ))
+	do
+		if [ -f "${arr1[$i]}/deploy.json" ]
+		then
+		type=`jq -r .deployment_type ${arr1[$i]}/deploy.json`
+			if [ -f /etc/nginx/sites-available/\${arr1[\$i]}.conf ]
+			then
+				echo "File Exist"
+			else
+				sudo touch /etc/nginx/sites-available/\${arr1[\$i]}.conf 
+				if [ $type == react ]
+    				then
+       				portno=`jq -r .port_number ${arr1[$i]}/deploy.json`
+				servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+				servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+				cd ${arr1[$i]} 
+        			sudo tee -a  /etc/nginx/sites-available/\${arr1[\$i]}.conf >/dev/null << EOF
+        			server {
+				listen 80;
+				listen [::]:80;
+
+            			root /var/www/demo;
+
+				server_name $servname;
+				location / {
+                    		proxy_pass http://127.0.0.1:$portno/;
+				}
+				}
+        			server {
+				listen 443;
+				listen [::]:443;
+
+           			root /var/www/demo;
+
+				server_name $servname; 
+				location / {
+                        proxy_pass http://127.0.0.1:$portno/;
+				}
+			}               
+EOF
+        		cd
+        elif [ $type == node ]
+        then
+           filename=`jq -r .file_name ${arr1[$i]}/deploy.json`
+			nportno=`jq -r .port_number ${arr1[$i]}/deploy.json`
+			servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+			servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+			cd ${arr1[$i]}
 			sudo tee -a  /etc/nginx/sites-available/\${arr1[\$i]}.conf >/dev/null << EOF
         		server {
 			listen 80;
@@ -104,11 +113,38 @@ EOF
 				}
 			}               
 EOF
-		else
-			echo "Not an app"
+        cd
+        elif [ $type == html ]
+        then
+            servname=`jq -r .server_name ${arr1[$i]}/deploy.json`
+		servalias=`jq -r .server_alias ${arr1[$i]}/deploy.json`
+            sudo tee -a  /etc/nginx/sites-available/\${arr1[\$i]}.conf >/dev/null << EOF
+        		server {
+			listen 80;
+			listen [::]:80;
+
+            		root /var/www/demo;
+
+			server_name $servname;
+			location / {
+                    		proxy_pass http://127.0.0.1:$portno/;
+				}
+			}
+        		server {
+			listen 443;
+			listen [::]:443;
+
+           		root /var/www/demo;
+
+			server_name $servname; 
+			location / {
+                        proxy_pass http://127.0.0.1:$portno/;
+				}
+			}               
+EOF
 		fi
-	fi
-done
+fi
+	done
 
 ##uncomment to check for changing the directory
 systemctl restart nginx
